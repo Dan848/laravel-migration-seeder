6d617bc419dbf5b9e59d68e212bb2d1570092c07
Dan848 <daniele.ceravolo15@gmail.com>
2023-05-25 05:52:10 +0200

    better colors

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
-    <div class="footer mt-5 bg-secondary">
+    <div class="footer mt-5 bg-dark">
         <div class="container d-flex align-items-center justify-content-between py-4">
-            <div><button class="text-uppercase btn btn-dark">sign-up-now!</button></div>
-            <div class="d-flex align-items-center">
+            <div><button class="text-uppercase fw-semibold btn btn-warning">sign up now!</button></div>
+            <div class="d-flex align-items-center text-white">
                 <h5 class="text-uppercase dc-text-primary my-0">follow us</h5>
                 <div class="ms-4 fs-4">
                     <i class="fa-brands fa-square-facebook"></i>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
-<header class="bg-secondary">
-    <nav class="navbar fw-bolder container navbar-expand-lg bg-body-tertiary">
+<header class="bg-warning bg-gradient">
+    <nav class="navbar fw-semibold container navbar-expand-lg">
         <div class="container-fluid">
             {{-- Logo --}}
             <a class="navbar-brand" href="{{route("home")}}">
@@ -16,16 +16,16 @@
                     @if (array_key_exists("dropdown", $links))
                         {{-- DropDown Menù --}}
                         <li class="nav-item dropdown">
-                            <div class="nav-link active dropdown-toggle text-white" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <div class="nav-link active dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             {{-- DropDown Title --}}
                                 {{$links["text"]}}
                             </div>
-                            <ul class="dropdown-menu bg-secondary">
+                            <ul class="dropdown-menu bg-warning">
                                 {{-- DropDown Links --}}
                                 @foreach ($links["dropdown"] as $dropLinks)
                                     <li>
                                         <a href="{{route("order", ["order" => $dropLinks["order"]])}}"
-                                        class="dropdown-item text-white">{{$dropLinks["text"]}}</a>
+                                        class="dropdown-item">{{$dropLinks["text"]}}</a>
                                     </li>
                                 @endforeach
                             </ul>
@@ -33,7 +33,7 @@
                     @else
                     {{-- Simple Anchor Link --}}
                         <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="{{route($links["href"])}}">
+                            <a class="nav-link active" aria-current="page" href="{{route($links["href"])}}">
                                 {{$links["text"]}}
                             </a>
                         </li>
